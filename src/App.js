46786e5cf90fd0a18c80c0acd5d7b69f06a54e2e
Jan030100 <gan.barshine@gmail.com>
2026-02-14 19:52:49 +0300

    Add useLocalStorage hook for data persistence

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>ToDo Mangment Tool</h1>
+      <p>tasks : {tasks.length}</p>
+    </div>
+  );
+}
+
+export default App;
